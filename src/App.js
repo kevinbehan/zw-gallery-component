25002486aea84c18bd57badbfda6af475c8e7a85
Kevin Behan <kevinbehan2017@gmail.com>
2018-06-26 03:23:39 +0000

    import test photos into app & photoswipe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import breadCat from './imgs/bread-cat.jpg'
+import flyingCat from './imgs/flying-cat.jpg'
+import monsterCat from './imgs/monster-cat.jpg'
+import {PhotoSwipeGallery} from 'react-photoswipe';
+import 'react-photoswipe/lib/photoswipe.css'
 import './App.css';
 
 class App extends Component {
