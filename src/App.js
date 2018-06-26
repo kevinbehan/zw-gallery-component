6d7a2017c065db3c065cbc4ee567dc3e81cba5e7
Kevin Behan <kevinbehan2017@gmail.com>
2018-06-26 03:31:02 +0000

    Create gallery component with photoswipe

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import breadCat from './imgs/bread-cat.jpg'
 import flyingCat from './imgs/flying-cat.jpg'
-import monsterCat from './imgs/monster-cat.jpg'
-import {PhotoSwipeGallery} from 'react-photoswipe';
+import monsterCat from './imgs/monster-cat.PNG'
+import {PhotoSwipeGallery} from 'react-photoswipe'
 import 'react-photoswipe/lib/photoswipe.css'
-import './App.css';
+import './App.css'
+
+let items = [
+  {
+    src: breadCat,
+    w: 1200,
+    h: 900,
+    title: 'Silly tabby cat manages to get his head stuck in a slice of bread',
+    msrc: breadCat,
+  },
+  {
+    src: flyingCat,
+    w: 1200,
+    h: 900,
+    title: 'Magical cat with paws stretched long in the midst of flying',
+    msrc: flyingCat,
+  },
+  {
+    src: monsterCat,
+    w: 1200,
+    h: 900,
+    title: 'Large cat that has taken over a website about HTML',
+    msrc: monsterCat,
+  }
+]
+
+let getThumbnailContent = item => <img width={120} height={90} src={item.msrc} alt={item.title}/>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+        <PhotoSwipeGallery items={items} thumbnailContent={getThumbnailContent} className="myGallery"/>
     );
   }
 }
